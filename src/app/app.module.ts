90abe86c4d1f7292b889c3dd38beb02fdb7c53e8
lee <iop2480@naver.com>
2019-01-07 21:52:13 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { ShowListComponent } from './show-list/show-list.component';
+import { ShowDetailComponent } from './show-detail/show-detail.component';
+import { WriteContentComponent } from './write-content/write-content.component';
+import { AppRoutingModule } from './app-routing.module';
+import { WriteContentService } from './services/write-content.service';
+import { ShowListService } from './services/show-list.service';
+import { AuthService } from './services/auth.service';
+import { ApiInterceptor } from './api.intercepter';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowListComponent,
+    ShowDetailComponent,
+    WriteContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ShowListService,
+    WriteContentService,
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
